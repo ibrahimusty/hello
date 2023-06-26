d994863563539a063bfb006d479eedd722b5ff02
ibrahim mustapha <ibrahimmustapha@ibrahims-MacBook-Pro.local>
2023-06-26 12:59:08 +0100

    habeeb

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('top g')
+    print_hi('habeeb')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
