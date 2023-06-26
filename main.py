15e779105f13da0ff176fd2ce0b80dfd40937ab1
ibrahim mustapha <ibrahimmustapha@ibrahims-MacBook-Pro.local>
2023-06-26 12:26:24 +0100

    first commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Ibrahim')
+    print_hi('fareedah')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
