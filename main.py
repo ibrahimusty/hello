586fec7aed87df8de8e8f10050261812bef89b70
Abdul <giwa.omotola@gmail.com>
2023-06-27 12:15:38 +0100

    test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('see')
+    print_hi('seejjj')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
